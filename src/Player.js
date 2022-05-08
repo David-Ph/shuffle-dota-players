7c69c11e2971ab4bad687035a88205fb81eab6b9
David <davidphang431@gmail.com>
2022-05-08 19:08:12 +0700

    Add initial class constructor

---
+++ src/Player.js
@@ -0,0 +1,6 @@
+export default class Player {
+  constructor(name, mmr) {
+    this.name = name;
+    this.mmr = mmr;
+  }
+}
---
+++ src/PlayerList.js
@@ -0,0 +1,5 @@
+export default class playersList {
+  constructor(players) {
+    this.players = players;
+  }
+}
