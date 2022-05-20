59dd0cea827202909a9b63f6f1e0400d8381b7b4
David - <55218137+David-Ph@users.noreply.github.com>
2022-05-20 22:04:58 +0700

    Copy to clipboard (#2)
    * Add icon to UI
    
    * Add copy to clipboard feature
    
    * Quick fix

--- src/utils/common.js
+++ src/utils/common.js
@@ -7,3 +7,7 @@ export const getRandomInt = (min, max) => {
 export const capitalizeString = (string) => {
   return string[0].toUpperCase() + string.substring(1);
 };
+
+export const copyToClipBoard = (text) => {
+  navigator.clipboard.writeText(text);
+};
